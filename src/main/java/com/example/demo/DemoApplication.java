c326ec72a5b5895cd73f5d8c369c4231e1c2cdc3
FengJiang <feng.jiang@helium10.com>
2024-09-24 13:15:46 +0800

    更改

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,7 +9,7 @@ public class DemoApplication {
     public static void main(String[] args) {
 
 
-        System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAaaa");
+        System.out.println("CC");
 
         SpringApplication.run(DemoApplication.class, args);
     }
